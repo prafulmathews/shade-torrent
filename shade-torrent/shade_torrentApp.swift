d1f4d47ae3ea72bafcc64c804eaf3fe4cfd28106
shade <99129435+prafulmathews@users.noreply.github.com>
2026-03-10 18:18:08 +0530

    Initial Commit

---
+++ shade-torrent/shade_torrentApp.swift
@@ -0,0 +1,17 @@
+//
+//  shade_torrentApp.swift
+//  shade-torrent
+//
+//  Created by Praful Mathews on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct shade_torrentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
